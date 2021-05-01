784e4ff234f046189d905131a98b30d52cb55603
Rodrigo Couto <rodrigo.couto.santos@hotmail.com>
2021-05-01 13:33:21 -0300

    updated

--- owner/blacklist.js
+++ owner/blacklist.js
@@ -3,12 +3,12 @@ const db = require('quick.db')
 
 exports.run = async (client, message, args) => {
 
-    let data = db.all().filter(i => i.ID.startsWith("blacklist_")).sort((a, b) => b.data - a.data);
+    let data = db.all().filter(i => i.ID.startsWith("blacklist_")).sort((a, b) => b.data - a.data)
     if (data.length < 1) return message.inlineReply("Sem ranking por enquanto").then(m => m.delete({ timeout: 5000 }))
 
     let myrank = data.map(m => m.ID).indexOf(`blacklist_${message.author.id}`) + 1 || "N/A"
-    data.length = 10;
-    let lb = [];
+    data.length = 10
+    let lb = []
     for (let i in data) {
         let id = data[i].ID.split("_")[1]
         let user = await client.users.fetch(id)
