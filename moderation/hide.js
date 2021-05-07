2e51da0f0cae12f7570e69dfa2dbf295be7d375b
Rodrigo Couto <rodrigo.couto.santos@hotmail.com>
2021-05-07 19:02:58 -0300

    new command

---
+++ moderation/hide.js
@@ -0,0 +1,18 @@
+const db = require("quick.db")
+
+exports.run = async (client, message, args) => {
+
+    if (!message.member.hasPermission("MANAGE_CHANNELS")) { return message.inlineReply("<:xis:835943511932665926> | Permissão necessária: Gerenciar Canais") }
+    if (!message.guild.me.hasPermission("MANAGE_CHANNELS")) { return message.inlineReply('<:xis:835943511932665926> | Eu preciso da permissão "Gerenciar Canais" para utilizar esta função.') }
+
+    let prefix = db.get(`prefix_${message.guild.id}`)
+    if (prefix === null) prefix = "-"
+
+    let canal = message.mentions.channels.first() || message.channel
+
+    if (args[1]) { return message.inlineReply("<:xis:835943511932665926> | Por favor, mencione apenas o canal que deseja esconder.") }
+
+    canal.updateOverwrite(message.guild.roles.cache.find(e => e.name.toLowerCase().trim() == "@everyone"), { VIEW_CHANNEL: false })
+
+    return message.channel.send(`🔒 ${message.author} escondeu o canal ${canal}! | Para mostrar o canal, use ` + '`' + prefix + 'unhide`')
+}
